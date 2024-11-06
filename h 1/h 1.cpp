bb7573e10df3d196b089960160486666dfd7b5c0
YourCrazyFan <bye6326076@gmail.com>
2024-11-06 22:32:55 +0300

    it calculates things

--- h 1/h 1.cpp
+++ h 1/h 1.cpp
@@ -9,7 +9,7 @@ int main()
     std::cout << "\033[92m-w- GREEN\033[0m\n";
     std::cout << "\033[96mTwT CYAN\033[0m\n";
     std::cout << "\033[94m>w< BLUE\033[0m\n";
-    std::cout << "\033[95mQwQ PURPLE\033[0m\n";
+    std::cout << "\033[95mQwQ PURPLE\033[0m\n\n";
 
     std::cout << "Give radius: ";
     double r;
@@ -19,6 +19,16 @@ int main()
     std::cin >> h;
     double const pi = 3.14159265;
     double v = pi * pow(r, 2) * h;
-    std::cout << "Volume: " << v;
+    std::cout << "Volume: " << v << "\n\n";
+
+    double number;
+    std::cout << "Gimme a number: ";
+    std::cin >> number;
+    int norm = number;
+    double decimal = number - double(norm);
+    std::cout << "Round -: " << norm << '\n';
+    std::cout << "Round +: " << (norm == number ? norm : norm+1) << '\n';
+    std::cout << "Round: " << (int(decimal * 10) < 5 ? norm : norm + 1) << '\n';
+    std::cout << "Decimal: " << decimal;
     return 0;
 }
