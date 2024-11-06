913922c97106c3d43216ca1f0a13bbb7145b4070
YourCrazyFan <bye6326076@gmail.com>
2024-11-06 21:59:26 +0300

    made it to optput colorful strings

---
+++ h 1/h 1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "\033[91m>w< RED\033[0m\n";
+    std::cout << "\033[33m-w- ORANGE\033[0m\n";
+    std::cout << "\033[93mOwO YELLOW\033[0m\n";
+    std::cout << "\033[92mUwU GREEN\033[0m\n";
+    std::cout << "\033[96mTwT CYAN\033[0m\n";
+    std::cout << "\033[94m>w< BLUE\033[0m\n";
+    std::cout << "\033[95mQwQ PURPLE\033[0m\n";
+
+    return 0;
+}
